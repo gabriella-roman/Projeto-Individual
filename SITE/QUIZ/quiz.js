d1c07f88dd11d70873da31cc3482b6aaa0fb1164
Gabriella Roman <gabriella.roman@sptech.school>
2023-05-18 11:28:05 -0300

    Testando Lógica

--- SITE/QUIZ/quiz.js
+++ SITE/QUIZ/quiz.js
@@ -70,13 +70,20 @@ var totalPerguntas = [
 
 var posicaoPergunta = 0
 
-/* INICIO DO QUIZ */
+/* INICIO DO QUIZ 
+*/
 function começar() {
+    var email = input_email_usuario.value;
+    var temArroba = email.indexOf("@")
+    var validacaoArroba = temArroba >= 0
+    var validacaoFimDoEmail = (email.endsWith("sptech.school") || email.endsWith(".com") || email.endsWith(".com.br"))
+    /*var validacaoSeEmailExisteNoBD = */
 
-    if (quiz.style.display == "none") {
+    if (quiz.style.display == "none" && validacaoFimDoEmail && validacaoFimDoEmail) {
         quiz.style.display = "flex"
     } else {
         quiz.style.display = "none"
+        spanErro.innerHTML = `Email inválido, verifique antes de continuar!`
     }
 }
 
@@ -84,7 +91,7 @@ function começar() {
 /* SISTEMA DE PONTUAÇÃO */
 function selecionarOpcao1() {
 
-    if (contador == 0) {
+    if (contador == 0 && (posicaoPergunta == 0 || posicaoPergunta == 3)) {
         mudarCor1.style.backgroundColor = "red"
         mudarCor2.style.backgroundColor = "#E48709"
         contador2 = 0
@@ -93,16 +100,18 @@ function selecionarOpcao1() {
         mudarCor4.style.backgroundColor = "#E48709"
         contador4 = 0
         contador++
+        respostaCerta = true
     } else {
         mudarCor1.style.backgroundColor = "#E48709"
         contador--
+        respostaCerta = false
     }
-    if (posicaoPergunta == 0) {
+    /*if (posicaoPergunta == 0) {
         respostaCerta = true
     }
     if (posicaoPergunta == 3) {
         respostaCerta = true
-    }
+    }*/
 }
 
 function selecionarOpcao2() {
@@ -122,7 +131,7 @@ function selecionarOpcao2() {
 }
 
 function selecionarOpcao3() {
-    if (contador3 == 0) {
+    if (contador3 == 0 && posicaoPergunta == 1) {
         mudarCor3.style.backgroundColor = "red"
         mudarCor1.style.backgroundColor = "#E48709"
         contador = 0
@@ -131,17 +140,17 @@ function selecionarOpcao3() {
         mudarCor4.style.backgroundColor = "#E48709"
         contador4 = 0
         contador3++
+        respostaCerta = true
     } else {
         mudarCor3.style.backgroundColor = "#E48709"
         contador3--
+        respostaCerta = false
     }
-    if (posicaoPergunta == 1) {
-        respostaCerta = true
-    }
+    
 }
 
 function selecionarOpcao4() {
-    if (contador4 == 0) {
+    if (contador4 == 0 && (posicaoPergunta == 2 || posicaoPergunta == 4)) {
         mudarCor4.style.backgroundColor = "red"
         mudarCor1.style.backgroundColor = "#E48709"
         contador = 0
@@ -150,16 +159,13 @@ function selecionarOpcao4() {
         mudarCor3.style.backgroundColor = "#E48709"
         contador3 = 0
         contador4++
+        respostaCerta = true
     } else {
         mudarCor4.style.backgroundColor = "#E48709"
         contador4--
+        respostaCerta = false
     }
-    if (posicaoPergunta == 2) {
-        respostaCerta = true
-    }
-    if (posicaoPergunta == 4) {
-        respostaCerta = true
-    }
+    
 }
 
 /* VOLTAR PARA PÁGINA INICIAL*/
